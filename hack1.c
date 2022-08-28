10e2ac82c60f305a812a5ad646cc93f98e2548a1
Aayush-cyborg <akulkarni3@huskers.unl.edu>
2022-08-28 12:44:18 +0000

    initial commit

---
+++ hack1.c
@@ -0,0 +1,17 @@
+/**
+ * Aayush kulkarni
+ * akulkarni3@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+
+int main(int argc, char **argv) {
+
+  printf("Aayush kulkarni!, Computer Engineering\n");
+  printf("My first lab!!!, Having funn\n");
+  printf("school of computing is great\n");
+
+  return 0;
+}
