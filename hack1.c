a65f7cbd83d85e66a03a21611b81add9b372415b
Aayushtech28 <69672911+Aayushtech28@users.noreply.github.com>
2022-08-29 09:01:01 +0000

    adding some info ormation

--- hack1.c
+++ hack1.c
@@ -10,8 +10,12 @@
 
 int main(int argc, char **argv) {
 
-  printf("Aayush kulkarni!, Computer Engineering\n");
+
+ printf("Aayush kulkarni!, Computer Engineering\n");
+  printf("This is second account of Aayush kulkarni!\n");
+  printf("I have so happy learning new things in git \n");
   printf("It's my first HACK-1 soo excitedd\n");
+  printf("I'll be completing my first HACK alone\n");
   printf("hello guys\n");
 
   return 0;
